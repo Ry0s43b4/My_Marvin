461bb27adb906b3fc4241383cabc8656a7350755
Nicky Truong <nicky.truong@epitech.eu>
2026-07-13 16:24:48 +0200

    feat: Finished the file by adding freeStylejobs asked (means that they are the most basic type of job and have a GUI to configure and automate tasks): clone-repository: That will clone the repository in a new folder (its job is located in Tools) SEED: Can basically build the project to be tested and have a SCM poll (Source Code Management) triggered every minute to check any changes or can be made manually

---
+++ job_dsl.groovy
@@ -0,0 +1,59 @@
+folder('Tools') {
+    description('Folder for miscellaneous tools.')
+}
+
+
+freeStyleJob('Tools/clone-repository') {
+    parameters {
+        stringParam('GIT_REPOSITORY_URL', '', 'Git URL of the repository to clone')
+    }
+    triggers {
+    }
+    wrappers {
+        preBuildCleanup()
+    }
+    steps {
+        shell('git clone $GIT_REPOSITORY_URL .')
+    }
+
+}
+
+freeStyleJob('Tools/SEED') {
+    parameters {
+        stringParam('GITHUB_NAME', '', 'GitHub repository owner/repo_name (e.g.: "EpitechIT31000/chocolatine")')
+        stringParam('DISPLAY_NAME', '', 'Display name for the job')
+    }
+    triggers {
+    }
+    steps {
+        dsl {
+            text('''
+                freeStyleJob(DISPLAY_NAME) {
+                    properties {
+                        githubProjectUrl("https://github.com/" + GITHUB_NAME + "/")
+                    }
+                    scm {
+                        git {
+                            remote {
+                                url("https://github.com/" + GITHUB_NAME + ".git")
+                            }
+                            branch("/main")
+                        }
+                    }
+                    triggers {
+                        scm(" * * * *")
+                    }
+                    wrappers {
+                        preBuildCleanup()
+                    }
+                    steps {
+                        shell("make fclean")
+                        shell("make")
+                        shell("make tests_run")
+                        shell("make clean")
+                    }
+                }
+            ''')
+        }
+    }
+}
