a5337f75b3a77fdc07da3a5a93f190841847ed61
Nicky Truong <nicky.truong@epitech.eu>
2026-07-13 16:43:07 +0200

    fix: Rewrite the file by removing useless variables and made it more clean

--- job_dsl.groovy
+++ job_dsl.groovy
@@ -1,13 +1,12 @@
-folder('Tools') {
+folder('Tools')
+{
     description('Folder for miscellaneous tools.')
 }
 
-
-freeStyleJob('Tools/clone-repository') {
+freeStyleJob('Tools/clone-repository')
+{
     parameters {
         stringParam('GIT_REPOSITORY_URL', '', 'Git URL of the repository to clone')
-    }
-    triggers {
     }
     wrappers {
         preBuildCleanup()
@@ -15,15 +14,13 @@ freeStyleJob('Tools/clone-repository') {
     steps {
         shell('git clone $GIT_REPOSITORY_URL .')
     }
-
 }
 
-freeStyleJob('Tools/SEED') {
+freeStyleJob('Tools/SEED')
+{
     parameters {
         stringParam('GITHUB_NAME', '', 'GitHub repository owner/repo_name (e.g.: "EpitechIT31000/chocolatine")')
         stringParam('DISPLAY_NAME', '', 'Display name for the job')
-    }
-    triggers {
     }
     steps {
         dsl {
